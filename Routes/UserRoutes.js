b3bd69f0741331aa8ba38c384aee2fab4a718d70
Basil Dayigil <97135216+Vasakee@users.noreply.github.com>
2024-07-26 23:22:50 +0100

    Update UserRoutes.js

--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -2,6 +2,6 @@ const express = require('express')
 const AuthUser = require('../Controllers/AuthController')
 const UserRoutes = express.Router()
 
-UserRoutes.get('/login', AuthUser)
+UserRoutes.post('/login', AuthUser)
 
 module.exports = UserRoutes
